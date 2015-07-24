f52fd04c9946f184cc78c81a5fe835878bddd89e
Serge Victor <github@random.re>
2015-07-24 18:45:57 +0700

    - bootstrap work in progress

---
+++ frontend.py
@@ -0,0 +1,42 @@
+# This contains our frontend; since it is a bit messy to use the @app.route
+# decorator style when using application factories, all of our routes are
+# inside blueprints. This is the front-facing blueprint.
+#
+# You can find out more about blueprints at
+# http://flask.pocoo.org/docs/blueprints/
+
+from flask import Blueprint, render_template
+from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
+
+from nav import nav
+
+frontend = Blueprint('frontend', __name__)
+
+# We're adding a navbar as well through flask-navbar. In our example, the
+# navbar has an usual amount of Link-Elements, more commonly you will have a
+# lot more View instances.
+nav.register_element('frontend_top', Navbar(
+    View('Flask-Bootstrap', '.index'),
+    View('Home', '.index'),
+    View('Debug-Info', 'debug.debug_root'),
+    Subgroup(
+        'Docs',
+        Link('Flask-Bootstrap', 'http://pythonhosted.org/Flask-Bootstrap'),
+        Link('Flask-AppConfig', 'https://github.com/mbr/flask-appconfig'),
+        Link('Flask-Debug', 'https://github.com/mbr/flask-debug'),
+        Separator(),
+        Text('Bootstrap'),
+        Link('Getting started', 'http://getbootstrap.com/getting-started/'),
+        Link('CSS', 'http://getbootstrap.com/css/'),
+        Link('Components', 'http://getbootstrap.com/components/'),
+        Link('Javascript', 'http://getbootstrap.com/javascript/'),
+        Link('Customize', 'http://getbootstrap.com/customize/'),
+    )
+))
+
+
+# Our index-page just shows a quick explanation. Check out the template
+# "templates/index.html" documentation for more details.
+@frontend.route('/')
+def index():
+    return render_template('index.html')
